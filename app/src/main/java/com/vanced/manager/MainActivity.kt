50537b547706487a22f1bf126dc0761abaa9a8a1
X1nto <burtkni@yandex.ru>
2020-03-17 18:55:24 +0400

    update

--- app/src/main/java/com/vanced/manager/MainActivity.kt
+++ app/src/main/java/com/vanced/manager/MainActivity.kt
@@ -15,11 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController = findNavController(R.id.mobile_navigation)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
