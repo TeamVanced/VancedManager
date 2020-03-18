85a0892098c4c9222fd88bfc983b40526678bad7
X1nto <burtkni@yandex.ru>
2020-03-18 22:10:54 +0400

    UI update

--- app/src/main/java/com/vanced/manager/MainActivity.kt
+++ app/src/main/java/com/vanced/manager/MainActivity.kt
@@ -3,24 +3,45 @@ package com.vanced.manager
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var homeFragment: HomeFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.navigation_home)
 
-        val navController = findNavController(R.id.mobile_navigation)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val navView : BottomNavigationView = findViewById(R.id.bottom_nav)
+
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        navView.setOnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+
+                R.id.home -> {
+
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+            }
+
+            true
+        }
     }
 }
--- app/src/main/java/com/vanced/manager/ui/home/HomeFragment.kt
+++ app/src/main/java/com/vanced/manager/ui/home/HomeFragment.kt
@@ -22,7 +22,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val textView: TextView = root.findViewById(R.id.Home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
