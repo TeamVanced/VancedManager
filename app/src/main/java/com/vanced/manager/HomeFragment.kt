ecb94aac0e26706744c08ef6392b8b4d2a36a032
X1nto <burtkni@yandex.ru>
2020-03-19 23:55:00 +0400

    Added working buttons and changed home fragment to RelativeLayout

--- app/src/main/java/com/vanced/manager/HomeFragment.kt
+++ app/src/main/java/com/vanced/manager/HomeFragment.kt
@@ -1,11 +1,22 @@
 package com.vanced.manager
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.browser.customtabs.CustomTabsClient
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsCallback
+import androidx.browser.customtabs.CustomTabsService
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.flow.DEFAULT_CONCURRENCY
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +28,27 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val braveurl = "https://brave.com/van874"
+        val vancedurl = "vanced.app"
+        val builder = CustomTabsIntent.Builder()
+        val brave = getView()?.findViewById(R.id.button) as Button
+        val website = getView()?.findViewById(R.id.button2) as Button
+
+        super.onViewCreated(view, savedInstanceState)
+
+        brave.setOnClickListener{
+            builder.setToolbarColor(ContextCompat.getColor(requireContext(), R.color.Vanced))
+           val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse(braveurl))
+        }
+        website.setOnClickListener{
+            builder.setToolbarColor(ContextCompat.getColor(requireContext(), R.color.Brave))
+           val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse(vancedurl))
+        }
     }
 
 }
