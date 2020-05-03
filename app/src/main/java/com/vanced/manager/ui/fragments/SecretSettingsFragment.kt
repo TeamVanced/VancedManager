aefde31ecbb9eba8dd345701424d886e34e265da
X1nto <burtkni@yandex.ru>
2020-05-03 15:29:45 +0400

    fixed toolbar menu

--- app/src/main/java/com/vanced/manager/ui/fragments/SecretSettingsFragment.kt
+++ app/src/main/java/com/vanced/manager/ui/fragments/SecretSettingsFragment.kt
@@ -11,8 +11,10 @@ class SecretSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.secret_settings, rootKey)
         activity?.title = "Secret Settings"
+        setHasOptionsMenu(true)
 
     }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.dev_settings_menu, menu)
         super .onCreateOptionsMenu(menu, inflater)
