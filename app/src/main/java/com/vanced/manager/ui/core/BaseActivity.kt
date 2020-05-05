c27b072f296524819bd84fa5e50c3cbe7531b473
X1nto <burtkni@yandex.ru>
2020-05-05 13:06:28 +0400

    added transparent theme

--- app/src/main/java/com/vanced/manager/ui/core/BaseActivity.kt
+++ app/src/main/java/com/vanced/manager/ui/core/BaseActivity.kt
@@ -66,6 +66,7 @@ open class ThemeActivity : AppCompatActivity() {
                     Configuration.UI_MODE_NIGHT_NO -> setLightAccent(currentAccent)
                 }
             }
+            "TRANS" -> setTransAccent(currentAccent)
             else -> setLightAccent("Blue")
         }
     }
@@ -85,6 +86,15 @@ open class ThemeActivity : AppCompatActivity() {
             "Red" -> setTheme(R.style.LightTheme_Red)
             "Green" -> setTheme(R.style.LightTheme_Green)
             "Yellow" -> setTheme(R.style.LightTheme_Yellow)
+        }
+    }
+
+    private fun setTransAccent(accentColor: String) {
+        when (accentColor) {
+            "Blue" -> setTheme(R.style.TransparentTheme_Blue)
+            "Red" -> setTheme(R.style.TransparentTheme_Red)
+            "Green" -> setTheme(R.style.TransparentTheme_Green)
+            "Yellow" -> setTheme(R.style.TransparentTheme_Yellow)
         }
     }
 
