b6ea249dd7aa773e169b88598bd8f74bad08cda2
X1nto <burtkni@yandex.ru>
2020-05-17 11:47:41 +0400

    viewmodel test 2

--- app/src/main/java/com/vanced/manager/ui/fragments/HomeViewModel.kt
+++ app/src/main/java/com/vanced/manager/ui/fragments/HomeViewModel.kt
@@ -2,17 +2,16 @@ package com.vanced.manager.ui.fragments
 
 import android.app.Activity
 import android.content.pm.PackageManager
-import android.provider.Settings.Global.getString
 import androidx.lifecycle.ViewModel
 import com.vanced.manager.R
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel {
 
     private val pm: PackageManager? = Activity().packageManager
 
     //we need to check whether these apps are installed or not
-    val microgStatus = pm?.let { isPackageInstalled("com.mgoogle.android.gms", it) }
-    val vancedStatus = pm?.let { isPackageInstalled("com.vanced.android.youtube", it) }
+    val microgStatus: Boolean? = pm?.let { isPackageInstalled("com.mgoogle.android.gms", it) }
+    val vancedStatus: Boolean? = pm?.let { isPackageInstalled("com.vanced.android.youtube", it) }
 
     private fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
         return try {
@@ -24,7 +23,8 @@ class HomeViewModel: ViewModel() {
     }
 
     val microgInstalledTxt: String? = pm?.getPackageInfo("com.mgoogle.android.gms", 0)?.versionName
-    val vancedInstalledTxt: String? = pm?.getPackageInfo("com.vanced.android.youtube", 0)?.versionName
+    val vancedInstalledTxt: String? = pm?.getPackageInfo("com.vanced.android.youtube", 0)?.versionName.toString()
+
 
 
 }
