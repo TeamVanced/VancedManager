45aa99d30ade71ceefcaadadeb50b77d05afa404
X1nto <burtkni@yandex.ru>
2020-06-30 15:31:09 +0400

    service tests 2

--- app/src/main/java/com/vanced/manager/core/downloader/DownloadBroadcastReceiver.kt
+++ app/src/main/java/com/vanced/manager/core/downloader/DownloadBroadcastReceiver.kt
@@ -1,5 +1,6 @@
 package com.vanced.manager.core.downloader
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -8,11 +9,19 @@ import android.util.Log
 class DownloadBroadcastReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val type = intent?.getStringExtra("type")
-        when (type) {
-            "vanced" -> context?.stopService(Intent(context, VancedDownloadService::class.java))
-            "microg" -> context?.stopService(Intent(context, MicrogDownloadService::class.java))
+        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        when (intent?.getStringExtra("type")) {
+            "vanced" -> {
+                context.stopService(Intent(context, VancedDownloadService::class.java))
+                notificationManager.cancel(69)
+                Log.d("VMNotification", "Canceled vanced download")
+            }
+            "microg" -> {
+                notificationManager.cancel(420)
+                context.stopService(Intent(context, MicrogDownloadService::class.java))
+                Log.d("VMNotification", "Canceled microg download")
+            }
         }
-        Log.d("VMNotification", "Canceled $type download")
+
     }
 }
