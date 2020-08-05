e9695caf7694641cc969773168baadaac2eea310
X1nto <burtkni@yandex.ru>
2020-08-05 20:20:19 +0400

    ah yeah, It's all coming together

---
+++ app/src/main/java/com/vanced/manager/core/firebase/CloudMessaging.kt
@@ -0,0 +1,13 @@
+package com.vanced.manager.core.firebase
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class CloudMessaging: FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        Log.d("VMC", "Generated new token: $p0")
+    }
+
+}
