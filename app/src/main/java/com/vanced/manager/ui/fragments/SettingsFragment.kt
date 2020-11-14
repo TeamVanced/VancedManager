e126878035f34ab99e596d8389d34013f117815c
HaliksaR <fun.haliksar.devel@gmail.com>
2020-11-15 05:08:15 +0700

    SettingsFragment view binding, fixed code and add ext

--- app/src/main/java/com/vanced/manager/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/vanced/manager/ui/fragments/SettingsFragment.kt
@@ -1,59 +1,86 @@
 package com.vanced.manager.ui.fragments
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.ViewGroup
 import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.preference.PreferenceManager.getDefaultSharedPreferences
+import androidx.preference.PreferenceManager.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.perf.FirebasePerformance
 import com.vanced.manager.R
 import com.vanced.manager.adapter.GetNotifAdapter
+import com.vanced.manager.core.ext.showDialogRefl
 import com.vanced.manager.databinding.FragmentSettingsBinding
+import com.vanced.manager.ui.core.BindingFragment
 import com.vanced.manager.ui.dialogs.*
 import com.vanced.manager.utils.LanguageHelper.getLanguageFormat
 import java.io.File
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : BindingFragment<FragmentSettingsBinding>() {
 
-    private lateinit var binding: FragmentSettingsBinding
+    private companion object {
+
+        const val LIGHT = "Light"
+        const val DARK = "Dark"
+        const val BLUE = "Blue"
+        const val RED = "Red"
+        const val GREEN = "Green"
+        const val YELLOW = "Yellow"
+    }
+
     private val prefs by lazy { getDefaultSharedPreferences(requireActivity()) }
 
-    override fun onCreateView(
+    override fun binding(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ) = FragmentSettingsBinding.inflate(inflater, container, false)
+
+    override fun otherSetups() {
         setHasOptionsMenu(true)
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
-        return binding.root
+        bindData()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun bindData() {
+        with(binding) {
+            bindRecycler()
+            bindFirebase()
+            bindManagerVariant()
+            bindClearFiles()
+            bindManagerTheme()
+            bindManagerAccentColor()
+            bindManagerLanguage()
+            selectApps.setOnClickListener { showDialogRefl<SelectAppsDialog>() }
+        }
+    }
 
-        binding.notificationsRecycler.apply {
+    private fun FragmentSettingsBinding.bindRecycler() {
+        notificationsRecycler.apply {
             layoutManager = LinearLayoutManager(requireActivity())
             adapter = GetNotifAdapter(requireActivity())
         }
+    }
 
-        binding.firebase.setOnCheckedListener { _, isChecked ->
+    private fun FragmentSettingsBinding.bindFirebase() {
+        firebase.setOnCheckedListener { _, isChecked ->
             FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(isChecked)
             FirebasePerformance.getInstance().isPerformanceCollectionEnabled = isChecked
             FirebaseAnalytics.getInstance(requireActivity()).setAnalyticsCollectionEnabled(isChecked)
         }
+    }
 
-        binding.managerVariant.apply {
+    private fun FragmentSettingsBinding.bindManagerVariant() {
+        managerVariant.apply {
             prefs.getString("vanced_variant", "nonroot")?.let { setSummary(it) }
-            setOnClickListener {
-                ManagerVariantDialog().show(requireActivity().supportFragmentManager, "")
-            }
+            setOnClickListener { showDialogRefl<ManagerVariantDialog>() }
         }
-
-        binding.clearFiles.setOnClickListener {
+    }
+    private fun FragmentSettingsBinding.bindClearFiles() {
+        clearFiles.setOnClickListener {
             with(requireActivity()) {
                 listOf("vanced/nonroot", "vanced/root", "music/nonroot", "music/root", "microg").forEach { dir ->
                     File(getExternalFilesDir(dir)?.path.toString()).deleteRecursively()
@@ -61,47 +88,43 @@ class SettingsFragment : Fragment() {
                 Toast.makeText(this, getString(R.string.cleared_files), Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
+    private fun FragmentSettingsBinding.bindManagerTheme() {
         val themePref = prefs.getString("manager_theme", "System Default")
-        binding.managerTheme.apply {
+        managerTheme.apply {
             setSummary(
-                    when (themePref) {
-                        "Light" -> getString(R.string.theme_light)
-                        "Dark" -> getString(R.string.theme_dark)
-                        else -> getString(R.string.system_default)
-                    }
+                when (themePref) {
+                    LIGHT -> getString(R.string.theme_light)
+                    DARK  -> getString(R.string.theme_dark)
+                    else  -> getString(R.string.system_default)
+                }
             )
-            setOnClickListener {
-                ManagerThemeDialog().show(requireActivity().supportFragmentManager, "")
-            }
+            setOnClickListener { showDialogRefl<ManagerThemeDialog>() }
         }
+    }
 
+    private fun FragmentSettingsBinding.bindManagerAccentColor() {
         val accentPref = prefs.getString("manager_accent", "Blue")
-        binding.managerAccentColor.apply {
+        managerAccentColor.apply {
             setSummary(
-                    when (accentPref) {
-                        "Blue" -> getString(R.string.accent_blue)
-                        "Red" -> getString(R.string.accent_red)
-                        "Green" -> getString(R.string.accent_green)
-                        "Yellow" -> getString(R.string.accent_yellow)
-                        else -> getString(R.string.accent_purple)
-                    }
+                when (accentPref) {
+                    BLUE   -> getString(R.string.accent_blue)
+                    RED    -> getString(R.string.accent_red)
+                    GREEN  -> getString(R.string.accent_green)
+                    YELLOW -> getString(R.string.accent_yellow)
+                    else   -> getString(R.string.accent_purple)
+                }
             )
-            setOnClickListener {
-                ManagerAccentColorDialog().show(requireActivity().supportFragmentManager, "")
-            }
+            setOnClickListener { showDialogRefl<ManagerAccentColorDialog>() }
         }
+    }
 
+    private fun FragmentSettingsBinding.bindManagerLanguage() {
         val langPref = prefs.getString("manager_lang", "System Default")
-        binding.managerLanguage.apply {
-            setSummary(getLanguageFormat(requireActivity(), langPref!!))
-            setOnClickListener {
-                ManagerLanguageDialog().show(requireActivity().supportFragmentManager, "")
-            }
-        }
-
-        binding.selectApps.setOnClickListener {
-            SelectAppsDialog().show(requireActivity().supportFragmentManager, "")
+        managerLanguage.apply {
+            setSummary(getLanguageFormat(requireActivity(), requireNotNull(langPref)))
+            setOnClickListener { showDialogRefl<ManagerLanguageDialog>() }
         }
 
     }
@@ -113,5 +136,4 @@ class SettingsFragment : Fragment() {
         }
         super.onCreateOptionsMenu(menu, inflater)
     }
-
 }
