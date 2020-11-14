4d14ddf1c6b39a684722af2925c5058cccc94a96
HaliksaR <fun.haliksar.devel@gmail.com>
2020-11-15 04:43:20 +0700

    GrantRootFragment view binding

--- app/src/main/java/com/vanced/manager/ui/fragments/GrantRootFragment.kt
+++ app/src/main/java/com/vanced/manager/ui/fragments/GrantRootFragment.kt
@@ -3,52 +3,47 @@ package com.vanced.manager.ui.fragments
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.edit
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.preference.PreferenceManager.getDefaultSharedPreferences
+import androidx.preference.PreferenceManager.*
 import com.topjohnwu.superuser.Shell
 import com.vanced.manager.R
 import com.vanced.manager.databinding.FragmentGrantRootBinding
 import com.vanced.manager.ui.MainActivity
+import com.vanced.manager.ui.core.BindingFragment
 
-class GrantRootFragment : Fragment(), View.OnClickListener {
+class GrantRootFragment : BindingFragment<FragmentGrantRootBinding>() {
 
-    private lateinit var binding: FragmentGrantRootBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    override fun binding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_grant_root, container, false)
-        return binding.root
+    ) = FragmentGrantRootBinding.inflate(inflater, container, false)
+
+    override fun otherSetups() {
+        bindData()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.grantRootFinishFab.setOnClickListener(this)
-        binding.grantRootFab.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.grant_root_fab -> {
-                if (Shell.rootAccess()) {
-                    getDefaultSharedPreferences(requireActivity()).edit { putString("vanced_variant", "root") }
-                } else {
-                    Toast.makeText(requireActivity(), R.string.root_not_granted, Toast.LENGTH_SHORT).show()
-                }
-            }
-            R.id.grant_root_finish_fab -> {
-                val intent = Intent(requireActivity(), MainActivity::class.java)
-                intent.putExtra("firstLaunch", true)
-                startActivity(intent)
-                requireActivity().finish()
-            }
+    private fun bindData() {
+        with(binding) {
+            grantRootFinishFab.setOnClickListener { navigateToFirstLaunch() }
+            grantRootFab.setOnClickListener { grantRoot() }
         }
     }
 
+    private fun navigateToFirstLaunch() {
+        val intent = Intent(requireActivity(), MainActivity::class.java)
+        intent.putExtra("firstLaunch", true)
+        startActivity(intent)
+        requireActivity().finish()
+    }
+
+    private fun grantRoot() {
+        if (Shell.rootAccess()) {
+            getDefaultSharedPreferences(requireActivity()).edit { putString("vanced_variant", "root") }
+        } else {
+            Toast.makeText(requireActivity(), R.string.root_not_granted, Toast.LENGTH_SHORT).show()
+        }
+    }
 }
