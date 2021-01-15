bb53021d2a3e4aaf3d97f2f71b58a4ac9ad03940
X1nto <burtkni@yandex.ru>
2021-01-15 21:26:27 +0400

    added a try catch block

--- app/src/main/java/com/vanced/manager/utils/InternetTools.kt
+++ app/src/main/java/com/vanced/manager/utils/InternetTools.kt
@@ -1,8 +1,10 @@
 package com.vanced.manager.utils
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
@@ -12,6 +14,7 @@ import androidx.preference.PreferenceManager.getDefaultSharedPreferences
 import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
 import com.vanced.manager.R
+import com.vanced.manager.library.network.okhttp.interceptors.LOG_INTERCEPTOR
 import com.vanced.manager.utils.AppUtils.generateChecksum
 import com.vanced.manager.utils.Extensions.getDefaultPrefs
 import kotlinx.coroutines.Dispatchers
@@ -36,16 +39,21 @@ object InternetTools {
     //var braveTiers = MutableLiveData<JsonObject?>()
 
     fun openUrl(url: String, color: Int, context: Context) {
-        val customTabPrefs = getDefaultSharedPreferences(context).getBoolean("use_custom_tabs", true)
-        if (customTabPrefs) {
-            val builder = CustomTabsIntent.Builder()
-            val params = CustomTabColorSchemeParams.Builder().setToolbarColor(ContextCompat.getColor(context, color))
-            builder.setDefaultColorSchemeParams(params.build())
-            val customTabsIntent = builder.build()
-            customTabsIntent.intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            customTabsIntent.launchUrl(context, url.toUri())
-        } else
-            context.startActivity(Intent(Intent.ACTION_VIEW, url.toUri()).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        try {
+            val customTabPrefs = getDefaultSharedPreferences(context).getBoolean("use_custom_tabs", true)
+            if (customTabPrefs) {
+                val builder = CustomTabsIntent.Builder()
+                val params = CustomTabColorSchemeParams.Builder().setToolbarColor(ContextCompat.getColor(context, color))
+                builder.setDefaultColorSchemeParams(params.build())
+                val customTabsIntent = builder.build()
+                customTabsIntent.intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                customTabsIntent.launchUrl(context, url.toUri())
+            } else
+                context.startActivity(Intent(Intent.ACTION_VIEW, url.toUri()).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(context, R.string.error, Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun getFileNameFromUrl(url: String) = url.substring(url.lastIndexOf('/') + 1, url.length)
