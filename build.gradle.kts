fcdc91029392d576ac0733798990b614736121dc
X1nto <burtkni@yandex.ru>
2021-01-22 20:15:47 +0400

    updated gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
         classpath("com.google.gms:google-services:4.3.4")
         classpath("com.google.firebase:perf-plugin:1.3.4")
