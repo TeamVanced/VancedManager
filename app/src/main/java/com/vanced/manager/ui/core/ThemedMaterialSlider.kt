8643ced9f484ae46d610b4cfc3525881e7dfdd4c
X1nto <burtkni@yandex.ru>
2021-02-03 23:43:41 +0400

    fixed accent color for slider

--- app/src/main/java/com/vanced/manager/ui/core/ThemedMaterialSlider.kt
+++ app/src/main/java/com/vanced/manager/ui/core/ThemedMaterialSlider.kt
@@ -7,14 +7,18 @@ import com.google.android.material.slider.Slider
 import com.vanced.manager.utils.defPrefs
 import com.vanced.manager.utils.managerAccent
 
-class ThemedMaterialSlider@JvmOverloads constructor(
+class ThemedMaterialSlider @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : Slider(context, attributeSet, defStyleAttr) {
 
     init {
-        thumbStrokeColor = ColorStateList.valueOf(context.defPrefs.managerAccent)
+        val accentValue = ColorStateList.valueOf(context.defPrefs.managerAccent)
+        thumbTintList = accentValue
+        trackActiveTintList = accentValue
+        trackInactiveTintList = accentValue.withAlpha(70)
+        haloTintList = accentValue.withAlpha(60)
     }
 
 }
