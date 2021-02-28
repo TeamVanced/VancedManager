22d67b51c336a8d8b2cc668bdd0c9aee865e3d54
X1nto <burtkni@yandex.ru>
2021-02-28 17:29:10 +0400

    bumped up kotlin to 1.4.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlin_version by extra("1.4.30")
+    val kotlinVersion by extra("1.4.31")
     repositories {
         google()
         jcenter()
@@ -9,7 +9,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.3.5")
         classpath("com.google.firebase:perf-plugin:1.3.4")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.0")
