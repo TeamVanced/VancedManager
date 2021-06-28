aa3234a83c25ff4294a830276e6d941840c85d83
X1nto <burtkni@yandex.ru>
2021-06-28 19:22:55 +0400

    progressbar adjustments

--- app/src/main/java/com/vanced/manager/ui/components/progressindicator/ManagerProgressIndicator.kt
+++ app/src/main/java/com/vanced/manager/ui/components/progressindicator/ManagerProgressIndicator.kt
@@ -1,12 +1,30 @@
 package com.vanced.manager.ui.components.progressindicator
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
 import com.vanced.manager.ui.components.color.managerAccentColor
+
+private val progressBarModifier = Modifier.composed {
+    then(
+        fillMaxWidth()
+    ).then(
+        clip(MaterialTheme.shapes.medium)
+    )
+}
+
 
 @Composable
 fun ManagerProgressIndicator() {
     LinearProgressIndicator(
+        modifier = progressBarModifier,
         color = managerAccentColor()
     )
 }
@@ -15,8 +33,13 @@ fun ManagerProgressIndicator() {
 fun ManagerProgressIndicator(
     progress: Float
 ) {
+    val animatedProgress by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    )
     LinearProgressIndicator(
+        modifier = progressBarModifier,
         color = managerAccentColor(),
-        progress = progress
+        progress = animatedProgress
     )
 }
