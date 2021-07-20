ba2b5a9150d1ea9084888a204f0931043ecc0e65
X1nto <burtkni@yandex.ru>
2021-07-20 15:26:57 +0400

    removed navigation library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,13 +105,13 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.3.0-rc02")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha02")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
 
     implementation("com.google.accompanist:accompanist-swiperefresh:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-placeholder-material:$accompanistVersion")
 
-   implementation("io.coil-kt:coil-compose:1.3.0")
+    implementation("io.coil-kt:coil-compose:1.3.0")
 
     implementation("com.github.skydoves:orchestra-colorpicker:$orchestraVersion")
 
