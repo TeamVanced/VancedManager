7c1392de940b2156e2836f5b51c0caa947350ed1
X1nto <burtkni@yandex.ru>
2021-10-11 15:01:07 +0400

    preheat the shell in splash screen

--- app/src/main/java/com/vanced/manager/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/vanced/manager/ui/SplashScreenActivity.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager.getDefaultSharedPreferences
+import com.topjohnwu.superuser.Shell
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (getDefaultSharedPreferences(this).getBoolean("firstLaunch", true)) {
-            startActivity(Intent(this, WelcomeActivity::class.java))
-            finish()
-        } else {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+        //Preheat the shell
+        Shell.getShell {
+            if (getDefaultSharedPreferences(this).getBoolean("firstLaunch", true)) {
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
         }
 
     }
