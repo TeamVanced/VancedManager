f3ebe85e301694a68e5317ad77ce895a201417d9
X1nto <burtkni@yandex.ru>
2021-11-10 17:40:57 +0400

    update dialog UI

--- app/src/main/java/com/vanced/manager/ui/component/dialog/ManagerDialog.kt
+++ app/src/main/java/com/vanced/manager/ui/component/dialog/ManagerDialog.kt
@@ -1,18 +1,13 @@
 package com.vanced.manager.ui.component.dialog
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import com.vanced.manager.ui.component.card.ManagerCard
 import com.vanced.manager.ui.component.text.ManagerText
 import com.vanced.manager.ui.theme.LargeShape
-import com.vanced.manager.ui.util.DefaultContentPaddingHorizontal
-import com.vanced.manager.ui.util.DefaultContentPaddingVertical
 
 @Composable
 fun ManagerDialog(
@@ -28,7 +23,9 @@ fun ManagerDialog(
         modifier = modifier,
         title = {
             ManagerText(
-                text = title
+                modifier = Modifier.fillMaxWidth(),
+                text = title,
+                textAlign = TextAlign.Center
             )
         },
         text = content,
